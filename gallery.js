90a76b2567d310938aadaf9f00913a32736b4086
Andrey <mergotgy1@gmail.com>
2024-08-25 17:55:08 +0300

    added adaptive interface

--- gallery.js
+++ gallery.js
@@ -7,10 +7,13 @@ const VIEWER = document.getElementById("viewer")
 
 const GALLERY = document.querySelector("div.gallery")
 
-let counter, folders = GALLERY.querySelectorAll("div.folder"), images
+const DETECT_THRESHOLD = 70
+
+let counter, folders = GALLERY.querySelectorAll("div.folder"), images, xCoord
 
 function showViewer() {
     folders.forEach((folder) => {
+        observer.observe(folder.querySelector("img.icon"))
         folder.addEventListener('click', () =>
         {
             counter = 0
@@ -21,20 +24,26 @@ function showViewer() {
     });  
 }
 
-NEXT_BUTTON.onclick = function() {
+VIEW_IMAGE.addEventListener('touchstart', handleTouchStart)
+VIEW_IMAGE.addEventListener('touchmove', handleTouchMove)
+
+function nextImage() {
+    xCoord = undefined
     if(counter < images.length - 1) {
         counter++
     }
     else {
         counter = 0
     }
-    images.forEach((image, index) => {
+    images.forEach((image, index) => {   
         if(counter == index) {
             VIEW_IMAGE.setAttribute("src", image.getAttribute("src"))
         }
     })
 }
-PREV_BUTTON.onclick = function() {
+
+function prevImage() {
+    xCoord = undefined
     if(counter > 0) {
         counter--
     }
@@ -48,8 +57,47 @@ PREV_BUTTON.onclick = function() {
     })
 }
 
+function handleTouchStart(event) {
+    event.preventDefault()
+    const firstTouch = event.touches[0]
+    xCoord = firstTouch.clientX
+}
+
+function handleTouchMove(event) {
+    event.preventDefault()
+    const xUp = event.touches[0].clientX
+    if(Math.abs(xCoord-xUp) > DETECT_THRESHOLD) {
+        if(xCoord > xUp) {            
+            nextImage()
+        }
+        else if(xCoord < xUp) {
+            prevImage()
+        }
+    }
+}
+
+NEXT_BUTTON.onclick = nextImage
+PREV_BUTTON.onclick = prevImage
+
 CLOSE_BUTTON.onclick = function() {
     VIEWER.close()
 }
+
+const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting && entry.target.src == "") {
+            entry.target.setAttribute("src", entry.target.getAttribute("data-src"))
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const options = {
+    root: document.viewport,
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 0,
+}
+
+const observer = new IntersectionObserver(callback, options)
 
 GALLERY.addEventListener('load', showViewer())
